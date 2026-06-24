ad2c7271fa0e861580c0717956e483f75408a376
서민종 <tjalswhd1@naver.com>
2026-06-24 14:31:47 +0900

    refactor : 프론트 배포 서버로 주소 변경

--- clab/src/api/axios.js
+++ clab/src/api/axios.js
@@ -4,7 +4,7 @@ import { authApi } from "./restApi"
 import { useAuthStore } from "@/stores/auth"
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://clab-api.site'
 })
 
 const noAuthUrls = [
